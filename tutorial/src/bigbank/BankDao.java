7ac5714b6b5e7658769cb2016634d90a784a6aa0
caiyingying <caiyingying234@gmail.com>
2013-10-26 16:06:38 +0800

    init...

---
+++ tutorial/src/bigbank/BankDao.java
@@ -0,0 +1,9 @@
+package bigbank;
+
+public abstract interface BankDao {
+	public abstract Account readAccount(Long paramLong);
+
+	public abstract void createOrUpdateAccount(Account paramAccount);
+
+	public abstract Account[] findAccounts();
+}
